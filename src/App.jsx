e946a504e81f8e574e364057226c807e21906122
juagilei <juagilei@gmail.com>
2024-02-01 05:20:56 +0100

    layout header, main, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { useState } from 'react'
+import Header from "./components/header"
+import Footer from "./components/footer"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header />
+    <main>Esto es el contenido principal</main>
+    <Footer />
+
     </>
   )
 }
