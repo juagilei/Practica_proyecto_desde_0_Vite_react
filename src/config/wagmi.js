b1762a98da53fe55dbf13a7fc8ecd465c4016d0c
juagilei <juagilei@gmail.com>
2024-02-05 20:14:09 +0100

    update

--- src/config/wagmi.js
+++ src/config/wagmi.js
@@ -5,7 +5,7 @@ import { sepolia } from 'wagmi/chains'
 export const config = createConfig(
   getDefaultConfig({
     // Required API Keys
-    alchemyId: import.meta.env.VITE_ALCHEMY_ID,
+    alchemyId: import.meta.env.VITE_ALCHEMY_PROJECT_ID,
     walletConnectProjectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
 
     // Required
