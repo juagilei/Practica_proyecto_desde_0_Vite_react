b9e8e9265f91e3266bb94dac641d40448ed872c1
juagilei <juagilei@gmail.com>
2024-02-05 06:34:57 +0100

    hook wagmi use account update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
-{/* Home lo voy a sustuir por min en App.jsx */}
-
+{/* Home lo voy a sustuir por main en App.jsx */}
+{/* Introduzco un hook de wagmi para que me indique el estado de la conexión de la wallet */}
+import { useAccount } from "wagmi";
 export default function Home() {
+        const { address, isConnecting, isDisconnected } = useAccount();
+        if (isConnecting) return <div>Connecting...</div>;
+        if (isDisconnected) return <div>Disconnected</div>;
     return(
     <div>
         <h1> Home </h1>
+        <div> Connected Wallet: {address} </div>
     </div>
     )
+
 }
