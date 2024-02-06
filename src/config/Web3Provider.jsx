1238a076d99db2f7dbd17cc637ac75b1b8c49068
juagilei <juagilei@gmail.com>
2024-02-06 09:41:31 +0100

    update

---
+++ src/config/Web3Provider.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types"
+import { WagmiProvider, createConfig } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
+import { mainnet } from "wagmi/chains";
+
+
+const config = createConfig(
+  getDefaultConfig({
+    chains: [mainnet],
+    appName: 'Blockmaker ERC20 TOKEN',
+    walletConnectProjectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+  })
+);
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider debugMode>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+Web3Provider.propTypes={
+  children: PropTypes.node.isRequired
+}
