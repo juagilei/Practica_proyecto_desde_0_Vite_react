052bd05f4f3759db3654113e96af69393db2ab88
juagilei <juagilei@gmail.com>
2024-02-06 10:32:56 +0100

    update TextInput

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -1,7 +1,22 @@
-export default function TextInput (){
-    return(
-        <div>
-            <p>TextInput</p>
-        </div>
-    )
+import PropTypes from 'prop-types'
+
+export default function TextInput({ type, onChange, value, placeholder, disabled }) {
+  return (
+    <input
+      type={type}
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder}
+      disabled={disabled}
+      className="border border-gray-300 rounded-md w-full px-3 py-2 outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent disabled:cursor-not-allowed"
+    />
+  )
+}
+
+TextInput.propTypes = {
+  type: PropTypes.oneOf(['text', 'password', 'email', 'number']),
+  onChange: PropTypes.func,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool
 }
